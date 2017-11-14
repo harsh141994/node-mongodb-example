3ff7a9bdf7f1454d5a7651ee62ec69498c2f0f82
Harsh <harsh141994@gmail.com>
2017-11-14 18:59:59 +0530

    callback hell and promise

--- operations.js
+++ operations.js
@@ -1,40 +1,21 @@
-const assert = require ('assert');
+const assert = require('assert');
 
-exports.insertDocument = (db, document, collection, callback) => { //db=mongodb database connection
-	//document that i want to insert
-	//collection in which i want to insert
-	//callback that will be called once the application is completed
+exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted " + result.result.n +
-            " documents into the collection " + collection);
-        callback(result);
-    });
+    return coll.insert(document);  //returning the promise
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);        
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result);        
-    });
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result);        
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
